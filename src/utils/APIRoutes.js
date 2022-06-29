ea4f47c78965c47b328dc2ecf9e39e450646f922
Jawhar Ghodbane <jawharghod@gmail.com>
2022-06-29 18:50:29 +0200

    set avatar done

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,8 +1,8 @@
 export const host = "http://localhost:5000";
 export const loginRoute = `${host}/user/login`;
 export const registerRoute = `${host}/user/register`;
+export const setAvatarRoute = `${host}/user/setavatar`;
 /*export const logoutRoute = `${host}/api/auth/logout`;
 export const allUsersRoute = `${host}/api/auth/allusers`;
 export const sendMessageRoute = `${host}/api/messages/addmsg`;
-export const recieveMessageRoute = `${host}/api/messages/getmsg`;
-export const setAvatarRoute = `${host}/api/auth/setavatar`;*/
+export const recieveMessageRoute = `${host}/api/messages/getmsg`;*/
